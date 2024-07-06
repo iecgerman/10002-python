22b0ae9d648e41599802cfb8e15e18af490f76d7
iecgerman <iec_german@hotmail.com>
2024-07-06 13:28:17 -0600

    clase03

--- clase03.py
+++ clase03.py
@@ -1,4 +1,44 @@
-print("HOLA")
+""" print("HOLA")
 print("MUNDO")
 
-print("CUAL ES TU NOMBRE?: \n\n    GERMAN")
+print("CUAL ES TU NOMBRE?: \n\n    GERMAN") 
+
+"""
+
+# con # puedes comentar sólo una línea
+
+nombre = 'german'
+
+print("tambien se pueden usar comillas simples ' ")
+
+'''
+Introducción a los conceptos básicos de programación:
+
+Variables
+Espacios en memoria para almacenar datos
+Tipos de Datos
+Enteros (int): Números sin decimales.
+Flotantes (float): Números con decimales.
+Cadenas (str): Texto.
+Booleanos (bool): True o False.
+
+Las variables no deben comenzar con números y no se deben usar palabras reservadas como:
+
+class
+if
+else
+for
+Pueden iniciar con _ o __ esto se usa mucho simbolizar que son variables de tipo privado.
+
+Algo importante a tener en cuenta es que en python se usa snake_case para el escribir variables.
+
+Python utiliza la indentación en lugar de llaves (como en otros lenguajes como C++ o Java) para definir bloques de código. Esta elección de diseño se basa en la filosofía de Python de hacer que el código sea legible y elegante. Aquí hay algunas razones clave:
+
+Claridad visual: La indentación obligatoria hace que la estructura del código sea más evidente. No hay necesidad de preocuparse por las llaves o paréntesis que se abren y cierran correctamente.
+Consistencia: En Python, todos los bloques de código (como condicionales, bucles y funciones) se definen mediante la indentación. Esto garantiza una coherencia en todo el código.
+Evita errores sutiles: En lenguajes con llaves, los errores pueden ocurrir si se olvida cerrar una llave o si se coloca incorrectamente. La indentación en Python reduce la posibilidad de estos errores.
+Estilo común: La comunidad Python sigue las mismas convenciones de indentación, lo que facilita la colaboración y la lectura del código de otros.
+La indentación en Python es una característica distintiva que promueve la legibilidad y la consistencia. Aunque algunos pueden encontrarlo restrictivo al principio, a largo plazo, muchos desarrolladores aprecian su simplicidad y coherencia.
+
+
+'''
