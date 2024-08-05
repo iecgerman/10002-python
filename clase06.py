1df03c5d23637bb99c18ae49d7e67eebd6896870
iecgerman <iec_german@hotmail.com>
2024-08-05 13:47:52 -0600

    clase 6

---
+++ clase06.py
@@ -0,0 +1,17 @@
+x = 10
+y = 5.34543
+# z = 1.2e6
+# a = 1.2E-6
+
+print(type(x))
+print(type(y))
+# print(z)
+# print(a)
+"""para comentar es con CTRL + C y para descomentar es con CTRL + U""" # <<<---- Tambien podemos seleccionar todo y poner 3 comillas al inicio y 3 comillas al final
+print(x + x)
+print(x + y)
+print(y + y)
+is_true = True
+is_false = False
+print(is_true)
+print(type(is_true))
