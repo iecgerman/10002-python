1dd5b5f1e38d8e66d8f7d2716c6163efdbb00183
iecgerman <iec_german@hotmail.com>
2024-08-06 03:19:02 -0600

    clase 8

---
+++ clase08.py
@@ -0,0 +1,49 @@
+# Operaciones Matemáticas en Python
+
+# Operadores numéricos
+
+a = 10
+b = 3
+
+print("Suma: ", a + b)
+print("Resta: ", a - b)
+print("Multiplicación:", a * b)
+print("División: ", a / b)
+
+print("Módulo o Residuo: ", a % b)
+
+print("Parte entera de la disvision: ", a//b)
+
+print("Potenciacion: ", a**b)
+
+# a = a + 2
+# a += 2
+# a -= 2
+# a *= 2
+a /= 3
+print("{:.2f}".format(a))
+
+"""El acrónimo PEMDAS es:
+
+P -> Paréntesis
+E -> Exponenciación
+M -> Multiplicación
+D -> División
+A -> Adición
+S -> Sustracción"""
+
+operation1 = 2 + 3 *4
+operation2 = (2 + 3) *4
+print(operation1)
+print(operation2)
+
+operation3 = (2+3) * (4**2) / 8-1
+
+print(operation3)
+
+print(a >= b)
+print(b <= b)
+
+print(a == b)
+print(a != b)
+
