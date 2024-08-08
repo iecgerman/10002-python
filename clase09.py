997a5003db0c16df295f2bb99fb4479b05e5c244
iecgerman <iec_german@hotmail.com>
2024-08-07 19:11:17 -0600

    clase 9

---
+++ clase09.py
@@ -0,0 +1,11 @@
+# Operaciones de Entrada/Salida en Consola
+
+name = input("Ingrese su nombre: ")
+print(name)
+print(type(name))
+age = int(input("Ingrese su edad: "))
+print(age)
+print(type(age))
+
+price = float(input("Ingrese el precio del articulo: "))
+print(type(price))
