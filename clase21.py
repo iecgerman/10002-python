a63d50eafebcca1d59f97e150e1680f35ec8e13b
iecgerman <iec_german@hotmail.com>
2024-09-01 21:23:05 -0600

    Clase 21 - ¿Cómo realizar una función recursiva en Python?

--- clase21.py
+++ clase21.py
@@ -2,15 +2,15 @@
 
 # ¿Cómo realizar una función recursiva en Python?
 
-# def factorial(n):    
-#     if n == 0:
-#         return 1
-#     else:
-#         return n * factorial(n-1)
+def factorial(n):    
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
     
-# factorial_5 = factorial(5)
+factorial_5 = factorial(5)
 
-# print(factorial_5)
+print(factorial_5) # 120
 
 
 # funcion de Fibonacci
@@ -27,13 +27,13 @@
 # print(fibonacci(number))
 
 #RETO SUMATORIA DE NUMEROS NATURALES
-def suma_de_numeros_naturales(n):
-    if n == 0:
-        return 0
-    else:
-        return n + suma_de_numeros_naturales(n-1)
+# def suma_de_numeros_naturales(n):
+#     if n == 0:
+#         return 0
+#     else:
+#         return n + suma_de_numeros_naturales(n-1)
 
-print(suma_de_numeros_naturales(4))
+# print(suma_de_numeros_naturales(4))
 
-for i in range(5):
-    print(f"la suma de numeros naturales hasta {i} es: {suma_de_numeros_naturales(i)}")
+# for i in range(5):
+#     print(f"la suma de numeros naturales hasta {i} es: {suma_de_numeros_naturales(i)}")
